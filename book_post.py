133ac2bf9d3bdc61f5c671bfae8179955862955c
waseemahmed123 <103589950+waseemahmed123@users.noreply.github.com>
2022-07-06 05:06:59 -0700

    Add files via upload

---
+++ book_post.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+BOOKS = {
+    "book_1": {"title": "Title One", "author": "Author One"},
+    "book_2": {"title": "Title One", "author": "Author One"},
+    "book_3": {"title": "Title Three", "author": "Author Three"},
+    "book_4": {"title": "Title Four", "author": "Author Four"},
+    "book_5": {"title": "Title Five", "author": "Author Five"}
+}
+
+
+@app.get("/")
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
+    return BOOKS
+
+
+@app.post("/")
+async def create_book(book_title, book_author):
+    current_book_id = 0
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split("_")[-1])
+            if x > current_book_id:
+                current_book_id = x
+    BOOKS[f"book_{current_book_id + 1}"] = {"title": book_title, "author": book_author}
+    return BOOKS[f"book_{current_book_id + 1}"]
