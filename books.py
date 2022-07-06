4ea341a25a03e4de9c729d5fa8a478d47b77987c
waseemahmed123 <103589950+waseemahmed123@users.noreply.github.com>
2022-07-06 01:38:48 -0700

    Add files via upload

---
+++ books.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+BOOKS = {
+    "book_1": {"title": "Title One", "author": "Author One"},
+    "book_2": {"title": "Title One", "author": "Author One"},
+    "book_3": {"title": "Title Three", "author": "Author Three"},
+    "book_4": {"title": "Title Four", "author": "Author Four"},
+    "book_5": {"title": "Title Five", "author": "Author Five"}
+}
+
+
+@app.get("/")
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
+    return BOOKS
+
+
+@app.get("/{book_name}")
+async def read_book(book_name: str):
+    return BOOKS[book_name]
